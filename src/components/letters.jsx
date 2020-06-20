03bd21ab2b9b6aebf81e6420544b16fab652fc46
Khongorzul <55127203+Khongorod@users.noreply.github.com>
2020-06-20 18:11:33 +0800

    khongorod (#13)
    Co-authored-by: Khongorzul <khongorzul@Khongorzuls-iMac.local>

--- src/components/letters.jsx
+++ src/components/letters.jsx
@@ -7,14 +7,11 @@ export const Letters = () => {
             <LetterCard letter="A" imageUrl="https://icons.iconarchive.com/icons/bingxueling/fruit-vegetables/256/apple-red-icon.png" />
             <LetterCard letter="B" imageUrl="https://icons.iconarchive.com/icons/custom-icon-design/flatastic-10/512/Bear-icon.png" />
             <LetterCard letter="C" imageUrl="https://www.festivalclaca.cat/pics/b/3/37445_candy-png.png" />
-<<<<<<< HEAD
             <LetterCard letter="H" imageUrl="https://i.pinimg.com/236x/1b/46/12/1b4612ce21e5aec16f82f7f527838690--safety-icons.jpg" />
             <LetterCard letter="N" imageUrl="https://cdn2.vectorstock.com/i/1000x1000/51/36/logo-letter-n-glitch-distortion-vector-22855136.jpg" />
             <LetterCard letter="D" imageUrl="https://w0.pngwave.com/png/676/381/rubber-duck-png-clip-art.png" />
             <LetterCard letter="R" imageUrl="https://i7.pngguru.com/preview/746/590/365/bugs-bunny-rabbit-drawing-cartoon-bunny-rabbit.jpg" />
-=======
             <LetterCard letter="D" imageUrl="https://w0.pngwave.com/png/676/381/rubber-duck-png-clip-art.png" />
->>>>>>> 950a07912e361174037a06d76dd8b6dc9a6825af
             <LetterCard letter="Y" imageUrl="https://www.clipartmax.com/png/middle/317-3178553_yoyo-clipart-png-yoyo-clipart.png" />
             <LetterCard letter="E" imageUrl="http://icons.iconarchive.com/icons/google/noto-emoji-animals-nature/128/22275-eagle-icon.png" />
             <LetterCard letter="Q" imageUrl="https://d2sbsqqdu2ofn4.cloudfront.net/media/images/QoP_350px.max-400x400.png"/>
