2143a4b7a495513ca88e1215a9b7ea67f71ffe03
Tushig <Tushig@Nests-iMac.local>
2020-06-20 14:51:48 +0800

    added the letter Y

--- src/components/letters.jsx
+++ src/components/letters.jsx
@@ -8,6 +8,7 @@ export const Letters = () => {
             <LetterCard letter="B" imageUrl="https://icons.iconarchive.com/icons/custom-icon-design/flatastic-10/512/Bear-icon.png" />
             <LetterCard letter="C" imageUrl="https://www.festivalclaca.cat/pics/b/3/37445_candy-png.png" />
             <LetterCard letter="D" imageUrl="https://w0.pngwave.com/png/676/381/rubber-duck-png-clip-art.png" />
+            <LetterCard letter="Y" imageUrl="https://www.clipartmax.com/png/middle/317-3178553_yoyo-clipart-png-yoyo-clipart.png" />
         </div>
     )
 }
