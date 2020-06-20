4d4dc1c4c3ff32bdcf5fd3b3a6509d4a442fa66e
Byambadorj Puntsag <byambadorjp@gmail.com>
2020-06-20 13:33:28 +0800

    Added padding on letter component

--- src/components/letter-card.jsx
+++ src/components/letter-card.jsx
@@ -4,10 +4,10 @@ export const LetterCard = (props) => {
     let { letter, imageUrl } = props;
 
     return (
-        <div className='flex justify-between items-start w-8 h-7 br-primary-2'>
+        <div className='flex justify-between items-start pa-3 w-8 h-7 br-primary-2'>
             <div className='fs-20'>{letter}</div>
             <div className="w100 h100" style={{
-                backgroundImage: `url(${imageUrl})`, 
+                backgroundImage: `url(${imageUrl})`,
                 backgroundSize: 'cover'
             }}></div>
         </div>
