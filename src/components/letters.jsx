238f2a1ec7164b7409b9a1f0edaaf9a60e7a0052
nest5 <nest5@Namuungvnjs-iMac.local>
2020-06-20 14:42:07 +0800

    Portos added E

--- src/components/letters.jsx
+++ src/components/letters.jsx
@@ -7,6 +7,7 @@ export const Letters = () => {
             <LetterCard letter="A" imageUrl="https://icons.iconarchive.com/icons/bingxueling/fruit-vegetables/256/apple-red-icon.png" />
             <LetterCard letter="B" imageUrl="https://icons.iconarchive.com/icons/custom-icon-design/flatastic-10/512/Bear-icon.png" />
             <LetterCard letter="C" imageUrl="https://www.festivalclaca.cat/pics/b/3/37445_candy-png.png" />
+            <LetterCard letter="E" imageUrl="http://icons.iconarchive.com/icons/google/noto-emoji-animals-nature/128/22275-eagle-icon.png" />
         </div>
     )
 }
