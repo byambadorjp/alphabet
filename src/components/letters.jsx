a4cf8de996f9c1fff5502366c39f28d1da669296
Byambadorj Puntsag <byambadorjp@gmail.com>
2020-06-20 12:58:52 +0800

    First commit

---
+++ src/components/letters.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { LetterCard } from '../components';
+
+export const Letters = () => {
+    return (
+        <div className='flex justify-evenly'>
+            <LetterCard letter="A" imageUrl="https://icons.iconarchive.com/icons/bingxueling/fruit-vegetables/256/apple-red-icon.png" />
+            <LetterCard letter="B" imageUrl="https://icons.iconarchive.com/icons/custom-icon-design/flatastic-10/512/Bear-icon.png" />
+        </div>
+    )
+}
