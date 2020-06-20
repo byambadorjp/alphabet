2ee99d5d4cbfc8c8ff21246ada33d231e93be27c
Zolboo <zolboo@nest-41s-iMac.local>
2020-06-20 15:14:04 +0800

    added wrap class

--- src/components/letters.jsx
+++ src/components/letters.jsx
@@ -3,7 +3,7 @@ import { LetterCard } from '../components';
 
 export const Letters = () => {
     return (
-        <div className='flex justify-evenly'>
+        <div className='flex justify-evenly wrap'>
             <LetterCard letter="A" imageUrl="https://icons.iconarchive.com/icons/bingxueling/fruit-vegetables/256/apple-red-icon.png" />
             <LetterCard letter="B" imageUrl="https://icons.iconarchive.com/icons/custom-icon-design/flatastic-10/512/Bear-icon.png" />
             <LetterCard letter="C" imageUrl="https://www.festivalclaca.cat/pics/b/3/37445_candy-png.png" />
