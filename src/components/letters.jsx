a3950cb83bd12e1131e6c2c99a68618f6005ccb5
Zolboo <zolboo@nest-41s-iMac.local>
2020-06-20 14:45:05 +0800

    added N letter

--- src/components/letters.jsx
+++ src/components/letters.jsx
@@ -7,6 +7,7 @@ export const Letters = () => {
             <LetterCard letter="A" imageUrl="https://icons.iconarchive.com/icons/bingxueling/fruit-vegetables/256/apple-red-icon.png" />
             <LetterCard letter="B" imageUrl="https://icons.iconarchive.com/icons/custom-icon-design/flatastic-10/512/Bear-icon.png" />
             <LetterCard letter="C" imageUrl="https://www.festivalclaca.cat/pics/b/3/37445_candy-png.png" />
+            <LetterCard letter="N" imageUrl="https://cdn2.vectorstock.com/i/1000x1000/51/36/logo-letter-n-glitch-distortion-vector-22855136.jpg"/>
         </div>
     )
 }
