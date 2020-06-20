299d6445dcded289c730673c396db54adb29cffc
nomin <nomin-14@nomins-iMac.local>
2020-06-20 14:57:48 +0800

    added letter P

--- src/components/letters.jsx
+++ src/components/letters.jsx
@@ -8,6 +8,7 @@ export const Letters = () => {
             <LetterCard letter="B" imageUrl="https://icons.iconarchive.com/icons/custom-icon-design/flatastic-10/512/Bear-icon.png" />
             <LetterCard letter="C" imageUrl="https://www.festivalclaca.cat/pics/b/3/37445_candy-png.png" />
             <LetterCard letter="D" imageUrl="https://w0.pngwave.com/png/676/381/rubber-duck-png-clip-art.png" />
+            <LetterCard letter="P" imageurl="https://i7.pngguru.com/preview/637/838/386/airplane-aircraft-plane-thumbnail.jpg" />
         </div>
     )
 }
