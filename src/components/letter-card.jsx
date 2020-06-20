e6a6b6ee20ade366e123a35cc52b5b87247e2102
Anand-Ochir <anand-ochir@Anand-Ochir.local>
2020-06-20 15:17:40 +0800

    border radius

--- src/components/letter-card.jsx
+++ src/components/letter-card.jsx
@@ -4,7 +4,7 @@ export const LetterCard = (props) => {
     let { letter, imageUrl } = props;
 
     return (
-        <div className='flex justify-between items-start pa-3 w-7 h-7 br-primary-2'>
+        <div className='flex justify-between items-start pa-3 w-7 h-7 br-primary-2 br-2'>
             <div className='fs-30'>{letter}</div>
             <div className="w100 h100" style={{
                 backgroundImage: `url(${imageUrl})`,
