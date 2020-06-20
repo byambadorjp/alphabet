c1d1ea7f3dbb892be8eedbb817d79483881df357
Khongorzul <khongorzul@Khongorzuls-iMac.local>
2020-06-20 16:57:42 +0800

    khongoroo

--- src/components/letters.jsx
+++ src/components/letters.jsx
@@ -8,10 +8,9 @@ export const Letters = () => {
             <LetterCard letter="B" imageUrl="https://icons.iconarchive.com/icons/custom-icon-design/flatastic-10/512/Bear-icon.png" />
             <LetterCard letter="C" imageUrl="https://www.festivalclaca.cat/pics/b/3/37445_candy-png.png" />
             <LetterCard letter="H" imageUrl="https://i.pinimg.com/236x/1b/46/12/1b4612ce21e5aec16f82f7f527838690--safety-icons.jpg" />
-            <LetterCard letter="N" imageUrl="https://cdn2.vectorstock.com/i/1000x1000/51/36/logo-letter-n-glitch-distortion-vector-22855136.jpg"/>
             <LetterCard letter="N" imageUrl="https://cdn2.vectorstock.com/i/1000x1000/51/36/logo-letter-n-glitch-distortion-vector-22855136.jpg" />
             <LetterCard letter="D" imageUrl="https://w0.pngwave.com/png/676/381/rubber-duck-png-clip-art.png" />
-            <LetterCard letter="R" imageUrl="https://i.pinimg.com/originals/08/79/b0/0879b0c01f6a5b658ce153fd2845ddf0.png" />
+            <LetterCard letter="R" imageUrl="https://w7.pngwing.com/pngs/686/86/png-transparent-rabbit-cartoon-cuteness-cartoon-cute-little-rabbit-cartoon-character-white-mammal.png" />
             <LetterCard letter="Y" imageUrl="https://www.clipartmax.com/png/middle/317-3178553_yoyo-clipart-png-yoyo-clipart.png" />
             <LetterCard letter="E" imageUrl="http://icons.iconarchive.com/icons/google/noto-emoji-animals-nature/128/22275-eagle-icon.png" />
             <LetterCard letter="Q" imageUrl="https://d2sbsqqdu2ofn4.cloudfront.net/media/images/QoP_350px.max-400x400.png"/>
@@ -19,4 +18,4 @@ export const Letters = () => {
             <LetterCard letter="W" imageUrl="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Ffree-png-zzckn&psig=AOvVaw0oAGCivI7fX7lilS2F_-aT&ust=1592722680785000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCODRhsboj-oCFQAAAAAdAAAAABAX" />
         </div>
     )
-}
+} 
