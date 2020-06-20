0d5e4f995369f6f6b7b9e18c8f963235b89af288
Bayarlakh <RBayarlakh@users.noreply.github.com>
2020-06-20 15:04:51 +0800

    Added letter K

--- src/components/letters.jsx
+++ src/components/letters.jsx
@@ -8,6 +8,7 @@ export const Letters = () => {
             <LetterCard letter="B" imageUrl="https://icons.iconarchive.com/icons/custom-icon-design/flatastic-10/512/Bear-icon.png" />
             <LetterCard letter="C" imageUrl="https://www.festivalclaca.cat/pics/b/3/37445_candy-png.png" />
             <LetterCard letter="D" imageUrl="https://w0.pngwave.com/png/676/381/rubber-duck-png-clip-art.png" />
+            <LetterCard letter="K" imageUrl="http://icons.iconarchive.com/icons/iconka/landmarks/128/kangaroo-icon.png" />
         </div>
     )
 }
