2df88208aaaf7af43c94572bdc4d24bc35a8f41b
Ermuun-Orgil <ermuun-orgil@bldoo.local>
2020-06-20 14:57:12 +0800

    letter-q

--- src/components/letters.jsx
+++ src/components/letters.jsx
@@ -8,6 +8,7 @@ export const Letters = () => {
             <LetterCard letter="B" imageUrl="https://icons.iconarchive.com/icons/custom-icon-design/flatastic-10/512/Bear-icon.png" />
             <LetterCard letter="C" imageUrl="https://www.festivalclaca.cat/pics/b/3/37445_candy-png.png" />
             <LetterCard letter="D" imageUrl="https://w0.pngwave.com/png/676/381/rubber-duck-png-clip-art.png" />
+            <LetterCard letter="Q" imageUrl="https://d2sbsqqdu2ofn4.cloudfront.net/media/images/QoP_350px.max-400x400.png"/>
         </div>
     )
 }
