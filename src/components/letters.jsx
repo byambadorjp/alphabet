9952bf3902b6b8ab15f5a25f071f31e49f49437d
Khongorzul <khongorzul@Khongorzuls-iMac.local>
2020-06-20 14:56:50 +0800

    letter-r

--- src/components/letters.jsx
+++ src/components/letters.jsx
@@ -8,6 +8,7 @@ export const Letters = () => {
             <LetterCard letter="B" imageUrl="https://icons.iconarchive.com/icons/custom-icon-design/flatastic-10/512/Bear-icon.png" />
             <LetterCard letter="C" imageUrl="https://www.festivalclaca.cat/pics/b/3/37445_candy-png.png" />
             <LetterCard letter="D" imageUrl="https://w0.pngwave.com/png/676/381/rubber-duck-png-clip-art.png" />
+            <LetterCard letter="R" imageUrl="https://i.pinimg.com/originals/08/79/b0/0879b0c01f6a5b658ce153fd2845ddf0.png"/>
         </div>
     )
 }
