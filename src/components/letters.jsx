dbe9f8852b1c586933d0a9de7ee4d397ec41e475
Anand-Ochir <anand-ochir@Anand-Ochir.local>
2020-06-20 14:55:21 +0800

    conflict

--- src/components/letters.jsx
+++ src/components/letters.jsx
@@ -7,11 +7,8 @@ export const Letters = () => {
             <LetterCard letter="A" imageUrl="https://icons.iconarchive.com/icons/bingxueling/fruit-vegetables/256/apple-red-icon.png" />
             <LetterCard letter="B" imageUrl="https://icons.iconarchive.com/icons/custom-icon-design/flatastic-10/512/Bear-icon.png" />
             <LetterCard letter="C" imageUrl="https://www.festivalclaca.cat/pics/b/3/37445_candy-png.png" />
-<<<<<<< HEAD
             <LetterCard letter="H" imageUrl="https://i.pinimg.com/236x/1b/46/12/1b4612ce21e5aec16f82f7f527838690--safety-icons.jpg" />
-=======
             <LetterCard letter="D" imageUrl="https://w0.pngwave.com/png/676/381/rubber-duck-png-clip-art.png" />
->>>>>>> 99fc4e94594a3c3c1aba58bbb9daf5fee6a4f984
         </div>
     )
 }
